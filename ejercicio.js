aa1cc2b14acb1829c353a755ea7a48bf29240f93
sofia <medinasofiaa|1@gmail.com>
2023-04-11 11:02:37 -0300

    Practica 1.7

---
+++ ejercicio.js
@@ -0,0 +1,13 @@
+
+
+let manzanaMariCant = 5;
+let manzanaPedroCant = 8;
+let manzanaSusanaCant = 9;
+
+let manzanasTotal = ( manzanaMariCant + manzanaPedroCant + manzanaSusanaCant) ;
+
+//console.log = " cantidad de manzanas totales " + manzanasTotal ;  
+
+console.log(" cantidad de manzanas totales " + manzanasTotal);
+
+
