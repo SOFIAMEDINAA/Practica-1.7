9feb52c6a0df07908c6d55d028206ce85c9df395
sofia <medinasofiaa|1@gmail.com>
2023-04-11 11:17:28 -0300

    Atualizacion ejercicio 2

---
+++ carpeta2/ejercicio2.js
@@ -0,0 +1,9 @@
+let magdalenasTotales = 28 ;
+let cantComensales = 13 ;
+let CantTotalManzanas = 0 ;
+ let magdalenasSobrantes = ( magdalenasTotales - cantComensales ) ;
+
+ console.log("magdalenas sobrantes: " + magdalenasSobrantes);
+
+ console.log( " quedaron " + CantTotalManzanas + " manzanas ");
+
